71dddc9e7678c5fda4d41115e659285733c583ca
James Zheng <jameszheng405@gmail.com>
2021-01-01 17:09:53 -0500

    completed project

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Square from "./Square";
 
-const Board = (/* { parameters } */) => (
+const Board = ({squares, onClick}) => {
   // TODO: Populate the board with squares
-  <></>
-);
+  const map = squares.map((value, i) => (<Square key={i} value={value} onClick={() => onClick(i)}/>));
+
+  return (
+    <div className='board' >
+      {map}
+    </div>
+    );
+  };
 
 export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,84 @@
 import React, { useState } from "react";
 import { calculateWinner } from "../calculate_win";
 import Board from "./Board";
+import Square from "./Square";
 
 const Game = () => {
   // TODO: Set up states and functions: position of Xs and Os on board,
   // step number, whether X is next, is there a win or tie, etc.
+  function Game() {
+    const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXisNext] = useState(true);
+      
+    let next = 'O';
+    if (xIsNext) {
+        next = 'X';
+    }
 
-  return (
-    <>
-      TODO: Render the board here along with the title, game status,
-      and 'Go to Start' button.
-    </>
+    function calculateWinner(board) {
+        const threes = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+        let winner = null;
+        threes.forEach(function(three) {
+            if (board[three[0]] === 'X' && board[three[1]] === 'X' && board[three[2]] === 'X') {
+                winner = 'X';
+                return;
+            } else if (board[three[0]] === 'O' && board[three[1]] === 'O' && board[three[2]] === 'O') {
+                winner = 'O';
+                return;
+            } else {
+                return;
+            }
+        })
+
+        return winner;
+    }
+
+    let winner = calculateWinner(board);
+
+    let handleClick = square => {
+        if (winner != null || board[square] != null) {
+            return;
+        } else {
+            board[square] = next;
+            setBoard(board);
+            setXisNext(!xIsNext);
+            setStepNumber(stepNumber + 1);
+        }
+    }; 
+
+    let jumpToStart = () => {
+        setBoard([null, null, null, null, null, null, null, null, null]);
+        setStepNumber(0);
+        setXisNext(true);
+    };
+
+    let result = board => {
+        if (winner != null) {
+            return "Winner: " + winner;
+        } else if (!board.includes(null)) {
+            return "Tie Game";
+        } else {
+            return "Next Player: " + next;
+        }
+    };
+
+    return (
+      <>
+         <h1>Tic Tac Toe</h1>
+         <Board squares={board} onClick={i => handleClick(i)} />
+         <div className='info-wrapper'>
+            <div>
+                <button onClick={() => jumpToStart()}>Go to Start</button>
+            </div>
+            <h3>{result(board)}</h3>
+         </div>
+      </>
   );
-};
 
+  }
+
+  return Game();  
+};
+  
 export default Game;
